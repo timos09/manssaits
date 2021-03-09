0f9f67c818f5f416eb1ff66aabd2402cf1c1897c
timos09 <64172477+timos09@users.noreply.github.com>
2021-03-09 15:01:00 +0200

    10lesson

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-const listItems = document.querySelectorAll('#book-list ul li');
+const list = document.querySelector('#book-list ul');
 
-Array.from(listItems).forEach(function(item){
-  item.addEventListener('click', (e) => {
-
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
     const li = e.target.parentElement;
-    console.log('child element to remove:', li);
-    console.log('parent element to remove child from:', li.parentElement);
     li.parentNode.removeChild(li);
-
-  });
-});
-
-// prevent default behaviour
-
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log('Navigation to', e.target.textContent, 'was prevented');
+  }
 });
