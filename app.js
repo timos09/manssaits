2406ad47420f56bbc5fcd51677c3318ccde4b25c
timos09 <64172477+timos09@users.noreply.github.com>
2021-03-09 14:54:57 +0200

    8lesson

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
-const banner = document.querySelector('#page-banner');
 const bookList = document.querySelector('#book-list');
 
-console.log('#page-banner node type is:', banner.nodeType);
-console.log('#page-banner node name is:', banner.nodeName);
-console.log('#page-banner has child nodes:', banner.hasChildNodes());
-console.log('book list parent element:', bookList.parentElement);
-console.log('book list parent node:', bookList.parentNode);
+console.log('#book-list next sibling:', bookList.nextSibling);
+console.log('#book-list next element sibling:', bookList.nextElementSibling);
+console.log('#book-list previous sibling:', bookList.previousSibling);
+console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
-console.log('all node children:');
-Array.from(bookList.childNodes).forEach(function(node){
-  console.log(node);
-});
-
-console.log('all element children:');
-Array.from(bookList.children).forEach(function(node){
-  console.log(node);
-});
-
-const titles = bookList.querySelectorAll('.name');
-
-console.log('Book titles:');
-Array.from(titles).forEach(function(title){
-  console.log(title.textContent);
-});
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!'
